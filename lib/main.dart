025a08d8c2301f7c7ded13c5fe7a26628df98781
Fodor Benedek <benedek@fodor.pro>
2023-01-03 18:05:35 +0100

    Routemaster behelyezése gyökérbe

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:routemaster/routemaster.dart';
 
 import 'book_provider.dart';
 import 'home_page.dart';
@@ -14,14 +15,15 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return MaterialApp.router(
       debugShowCheckedModeBanner: false,
       title: 'Énekeskönyv',
       theme: ThemeData.dark(useMaterial3: true),
-      home: ChangeNotifierProvider<BookProvider>(
-        create: (_) => BookProvider()..initialize(),
-        child: const MyHomePage(),
-      ),
+      routerDelegate: RoutemasterDelegate(routesBuilder: (context) => routes),
+      routeInformationParser: const RoutemasterParser(),
     );
   }
 }
+
+final routes =
+    RouteMap(routes: {'/': (route) => const MaterialPage(child: MyHomePage())});
